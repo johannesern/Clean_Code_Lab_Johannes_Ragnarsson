0348e83fbe9efecb5b8724c2e46eae8db1c0868c
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-08-21 20:21:24 +0200

    created test project and class

---
+++ GameTests/StatisticsTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clean_Code_Lab_Johannes_Ragnarsson;
+
+namespace Clean_Code_Lab_Johannes_Ragnarsson.Tests
+{
+    [TestClass()]
+    public class StatisticsTests
+    {
+        [TestMethod()]
+        public void SplitStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace GameTests
+{
+    [TestClass]
+    public class StatisticsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
