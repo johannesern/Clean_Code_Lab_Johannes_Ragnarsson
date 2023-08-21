9fa88a290510a62ce8e75f54855275de6e515bce
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-08-21 09:03:50 +0200

    Add project files.

---
+++ Clean_Code_Lab_Johannes_Ragnarsson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Clean_Code_Lab_Johannes_Ragnarsson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
