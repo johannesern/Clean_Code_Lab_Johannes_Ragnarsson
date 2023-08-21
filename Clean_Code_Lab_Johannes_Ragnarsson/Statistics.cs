be5655f2f78c28e1f94f051ce0784694bb4af32d
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-08-21 09:44:55 +0200

    created new class for statistics

---
+++ Clean_Code_Lab_Johannes_Ragnarsson/Statistics.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Code_Lab_Johannes_Ragnarsson
+{
+    public class Statistics
+    {
+        private static List<PlayerData> results = new List<PlayerData>();
+
+        public static void SaveResult(string playerName, int guesses)
+        {
+            PlayerData playerData = results.Find(p => p.Name == playerName);
+
+            if (playerData == null)
+            {
+                playerData = new PlayerData(playerName, guesses);
+                results.Add(playerData);
+            }
+            else
+            {
+                playerData.Update(guesses);
+            }
+        }
+
+        public static void ShowTopList()
+        {
+            StreamReader input = new StreamReader("result.txt");
+            List<PlayerData> results = new List<PlayerData>();
+            string line;
+            while ((line = input.ReadLine()) != null)
+            {
+                string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None);
+                string name = nameAndScore[0];
+                int guesses = Convert.ToInt32(nameAndScore[1]);
+                PlayerData pd = new PlayerData(name, guesses);
+                int pos = results.IndexOf(pd);
+                if (pos < 0)
+                {
+                    results.Add(pd);
+                }
+                else
+                {
+                    results[pos].Update(guesses);
+                }
+
+
+            }
+            results.Sort((p1, p2) => p1.Average().CompareTo(p2.Average()));
+            Console.WriteLine("Player   games average");
+            foreach (PlayerData p in results)
+            {
+                Console.WriteLine(string.Format("{0,-9}{1,5:D}{2,9:F2}", p.Name, p.NGames, p.Average()));
+            }
+            input.Close();
+        }
+    }
+
+    class PlayerData
+    {
+        public string Name { get; private set; }
+        public int NGames { get; private set; }
+        int totalGuess;
+
+
+        public PlayerData(string name, int guesses)
+        {
+            this.Name = name;
+            NGames = 1;
+            totalGuess = guesses;
+        }
+
+        public void Update(int guesses)
+        {
+            totalGuess += guesses;
+            NGames++;
+        }
+
+        public double Average()
+        {
+            return (double)totalGuess / NGames;
+        }
+
+
+        public override bool Equals(Object p)
+        {
+            return Name.Equals(((PlayerData)p).Name);
+        }
+
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
