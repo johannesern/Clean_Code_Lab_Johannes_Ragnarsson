f4c6c209002432f1e864d632f4dfdc68b0feb88a
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-08-21 09:36:47 +0200

    added UserInterface

---
+++ Clean_Code_Lab_Johannes_Ragnarsson/UserInterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Code_Lab_Johannes_Ragnarsson
+{
+    public class UserInterface
+    {
+        public static string GetPlayerName()
+        {
+            Console.WriteLine("Enter your user name:");
+            return Console.ReadLine();
+        }
+
+        public static string GetGuess()
+        {
+            return Console.ReadLine();
+        }
+
+        public static bool AskToContinue()
+        {
+            Console.WriteLine("Continue? (y/n)");
+            string answer = Console.ReadLine();
+            return !string.IsNullOrEmpty(answer) && answer.Trim().ToLower() == "y";
+        }
+    }
+}
