1b1e4a42631f4f5e630bbc75d55878e80d876a24
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-08-22 16:02:21 +0200

    new testclass and first test method

---
+++ GameTests/MooGameLogictests.cs
@@ -0,0 +1,29 @@
+﻿using Clean_Code_Lab_Johannes_Ragnarsson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTests
+{
+    [TestClass()]
+    public class MooGameLogictests
+    {
+        //Classname_MethodName_ExpectedResult
+        [TestMethod()]
+        public void MooGameLogic_CheckBullsAndCows_ReturnStringWithCorrectCownsAndBulls()
+        {
+            //Arrange
+            string goal = "1234";
+            string guess = "1243";
+            string correctBullsAndCows = "BB,CC";
+
+            //Act
+            var response = CheckBullsAndCows(goal, guess);
+
+            //Assert
+            Assert.AreEqual(correctBullsAndCows, response);
+        }
+    }
+}
