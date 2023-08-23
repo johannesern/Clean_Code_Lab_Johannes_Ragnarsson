8c24f8fdf750ee78bcec5b87e1933d0ecc3a6664
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-08-23 11:57:55 +0200

    new method to check userinput so that we dont receive empty guesses

--- Clean_Code_Lab_Johannes_Ragnarsson/GameController.cs
+++ Clean_Code_Lab_Johannes_Ragnarsson/GameController.cs
@@ -19,20 +19,20 @@
 
                 _game.Initialize();
 
-                string guess = UI.UserInput();
-                string bbcc = _game.CheckGuess(guess);
-                UI.Output(bbcc);
+                string userGuess = CheckUserInput();
+                string bullsAndCows = _game.CheckGuess(userGuess);
+                UI.Output(bullsAndCows);
 
-                while (bbcc != "BBBB,")
+                while (bullsAndCows != "BBBB,")
                 {
                     player.TotalGuesses++;
-                    guess = UI.UserInput();
-                    bbcc = _game.CheckGuess(guess);
-                    UI.Output(bbcc);
+                    userGuess = CheckUserInput();
+                    bullsAndCows = _game.CheckGuess(userGuess);
+                    UI.Output(bullsAndCows);
                 }
 
-                string response = Statistics.SaveResult(player, filename);
-                UI.Output(response);
+                string userUpdated = Statistics.SaveResult(player, filename);
+                UI.Output(userUpdated);
                 Statistics.ShowTopList(filename);
 
                 playOn = UI.AskToContinue();
@@ -44,8 +44,29 @@
             }
         }
 
+        private static string CheckUserInput()
+        {
+            bool isEmpty;
+            string userGuess;
+            do
+            {
+                userGuess = UI.Input();
+                isEmpty = String.IsNullOrEmpty(userGuess);
+                if (isEmpty)
+                {
+                    UI.Output("\nCannot be an empty guess, try again...\n");
+                }                
+
+            } while (isEmpty);
+
+            return userGuess;
+        }
+
         private string GetGameName()
         {
+            // For example, if _game is type of MooGameStrategy =>
+            // then GetType.Name = MooGameStrategy
+            // we remove Strategy and get our filename = MooGame.txt
             var gameName = _game.GetType().Name;
             var indexOfS = gameName.IndexOf('S');
             var filename = gameName.Substring(0, indexOfS) + ".txt";
