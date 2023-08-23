450ad12e22c1fa770231f25f32213e4b85641df2
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-08-23 09:56:03 +0200

    baseclass  removed test removed

--- GameTests/MooGameLogictests.cs
+++ GameTests/MooGameLogictests.cs
@@ -6,19 +6,6 @@ namespace GameTests
     public class MooGameLogictests
     {
         //Classname_MethodName_ExpectedResult
-        [TestMethod()]
-        public void MooGameLogic_CheckBullsAndCows_ReturnStringWithCorrectCownsAndBulls()
-        {
-            //Arrange
-            string goal = "1234";
-            string guess = "1243";
-            string correctBullsAndCows = "BB,CC";
-
-            //Act
-            var response = MooGameLogic.CheckBullsAndCows(goal, guess);
-
-            //Assert
-            Assert.AreEqual(correctBullsAndCows, response);
-        }
+        
     }
 }
