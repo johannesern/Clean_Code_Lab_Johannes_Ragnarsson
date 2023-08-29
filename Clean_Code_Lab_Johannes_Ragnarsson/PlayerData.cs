9648e0a953b9c4e7a5e01084ddda32642a6ca276
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2023-08-29 13:15:16 +0200

    refactor SRP, playerdata moved to own file

---
+++ Clean_Code_Lab_Johannes_Ragnarsson/PlayerData.cs
@@ -0,0 +1,38 @@
+﻿namespace Clean_Code_Lab_Johannes_Ragnarsson
+{
+    public class PlayerData
+    {
+        public string Name { get; set; }
+        public int NumberOfGames { get; set; }
+        public int TotalGuesses { get; set; }
+
+
+        public PlayerData(string name, int guesses)
+        {
+            this.Name = name;
+            NumberOfGames = 1;
+            TotalGuesses = guesses;
+        }
+
+        public PlayerData(string name, int numberOfGames, int totalGuesses)
+        {
+            this.Name = name;
+            NumberOfGames = numberOfGames;
+            TotalGuesses = totalGuesses;
+        }
+
+        //public PlayerData() { }
+
+
+        public void UpdateUserData(int guesses)
+        {
+            TotalGuesses += guesses;
+            NumberOfGames++;
+        }
+
+        public double AverageGuesses()
+        {
+            return (double)TotalGuesses / NumberOfGames;
+        }
+    }
+}
